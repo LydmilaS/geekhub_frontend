6c573ff9c71e06bd31fe236dc7b8dd0c5f759b5a
LydmilaS <lydmilazoloto@gmail.com>
2016-01-03 21:39:07 +0200

    google map

---
+++ homework11/js/map.js
@@ -0,0 +1,21 @@
+ var myCenter=new google.maps.LatLng(51.5226439, 0.0314533);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:15,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  icon:'images/map-marker.png'
+  });
+
+marker.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
